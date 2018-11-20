d1e8c86b1a8067f22716968e7ecec69cae248dd6
vagrant <vagrant@localhost.localdomain>
2018-11-20 14:34:23 +0000

    モデルの作成とマイグレーションの実行

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :fan_comments
+  resources :reviews
+  resources :authors
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
